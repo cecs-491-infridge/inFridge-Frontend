226cfc69be1a8f0c0bbc879f859c144f4334197d
Weisheng Wu <weishenggwuu@gmail.com>
2019-03-07 17:39:13 -0800

    added splash screen file

---
+++ components/FridgeList.js
@@ -0,0 +1,19 @@
+import { ListItem } from 'react-native-elements'
+
+class FridgeList extends React.Component{
+    
+}
+const list = [
+  {
+    name: 'Amy Farha',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Vice President'
+  },
+  {
+    name: 'Chris Jackson',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'Vice Chairman'
+  },
+]
+
+export default list;
